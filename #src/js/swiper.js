f614b82e94b9e69e20bad994f619384ad29c8aa2
SerhiiIkin <messileonl@gmail.com>
2022-08-25 16:22:42 +0200

    set swiper in portfolio project

--- #src/js/swiper.js
+++ #src/js/swiper.js
@@ -17,3 +17,23 @@ new Swiper(".portfolio", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+new Swiper(".project__mern", {
+    slidesPerView: 1,
+    spaceBetween: 400,
+    speed: 800,
+    grabCursor: true,
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 60,
+        slideShadows: false,
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+})
